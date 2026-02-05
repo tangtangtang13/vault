87b8b44f623ce55b944ea3f3756998c09e8c943e
tangtangtang13 <tangtangtang13cxzaq@gmail.com>
2026-02-06 05:40:09 +0700

    Delete js/db.js

--- js/db.js
+++
@@ -1,9 +0,0 @@
-export function openDB() {
-  return new Promise((res) => {
-    const req = indexedDB.open("vaultDB", 1);
-    req.onupgradeneeded = e => {
-      e.target.result.createObjectStore("items", { keyPath: "id", autoIncrement: true });
-    };
-    req.onsuccess = e => res(e.target.result);
-  });
-}
